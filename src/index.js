9789413a00b0c9f8015cf033c5b460a5d7499bfe
AListckii <sobbabobba@gmail.com>
2019-10-11 15:04:27 +0400

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var getBrackets = function(bracketsConfig) {
+        return bracketsConfig.map(function(elem) {
+          return elem.join('');   //все элементы из конфига в строку без пробелов
+        });
+      }    
+      var brackets = getBrackets(bracketsConfig);
+    
+   
+      for (let i = 0; i < brackets.length;) {
+          if (str.includes(brackets[i]) ) {
+            str = str.replace(brackets[i], '');  // если в строке есть элемент из конфига то заменяем и проверяем снова
+            i = 0; 
+          } else i++; // к следующему из конфига
+      };
+    
+      return str ? false : true;  // если в строке что-то осталось то ошибка
 }
